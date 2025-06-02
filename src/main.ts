4063b5b84e770833722e691ac2f115731cb45ca3
SoleneMarie <solene.plassart@gmail.com>
2025-06-02 15:29:26 +0200

    fix: main

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3001', // 👈 ton front
+    origin: true,
     credentials: true,
   });
   await app.listen(parseInt(process.env.PORT || '3000', 10));
